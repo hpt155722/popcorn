511d3fa8a0922afea171dc8f8fbdbc5d8833fa85
Jinn Ha <haphu155722@gmail.com>
2023-10-28 20:00:17 -0500

    added change pfp

---
+++ README.md
@@ -0,0 +1,47 @@
+# popcorn
+
+A text based social media platform.
+
+<br>
+
+### ☆ ───── Technologies ───── ☆
+
+<br>
+☆ HTML <br>
+☆ CSS <br>
+☆ Javascript <br>
+☆ PHP <br>
+☆ mySQL <br>
+<br>
+
+### ☆ ───── Features ───── ☆
+
+ <br>
+☆ Login and account creation page <br>
+☆ Create and delete text-based posts <br>
+☆ Image upload for profile picture <br>
+☆ Post has like feature <br>
+☆ Chronological feed <br>
+☆ Search engine for posts <br>
+☆ Notifications tab <br>
+☆ User profile page to view all of your posts and/or delete them <br>
+☆ Custom art <br>
+
+ <br>
+ 
+### ☆ ───── Screenshots ───── ☆
+
+ <br>
+ <div>
+    <img style = "width: 20vh; margin: 5vh;" src = "resources/readmeResources/login.png">
+    <img style = "width: 20vh; margin: 5vh;" src = "resources/readmeResources/signup.png">
+    <img style = "width: 20vh; margin: 5vh;" src = "resources/readmeResources/feed.png">
+    <img style = "width: 20vh; margin: 5vh;" src = "resources/readmeResources/community.png">
+    <img style = "width: 20vh; margin: 5vh;" src = "resources/readmeResources/profile.png">
+</div>
+ <br>
+ 
+### ☆ ───── Future enhancements ───── ☆
+
+ <br>
+☆ Follow feature <br>
--- utils/uploadPFP.php
+++ utils/uploadPFP.php
@@ -1,40 +1,46 @@
 <?php
-    session_start();
-    include("connection.php");
+session_start();
+include("connection.php");
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $currentUsername = $_SESSION['currUser'];
-        $croppedData = $_POST['croppedData'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $currentUsername = $_SESSION['currUser'];
+    $croppedData = $_POST['croppedData'];
 
-        // Extract just the base64 encoded data from the URI
-        $data = explode(',', $croppedData);
-        $croppedImage = base64_decode($data[1]);
+    // Extract just the base64 encoded data from the URI
+    $data = explode(',', $croppedData);
+    $croppedImage = base64_decode($data[1]);
 
-        if ($croppedImage !== false && $currentUsername) {
-            $filename = uniqid() . '.webp';
-            $filepath = '../resources/pfp/' . $filename;
+    if ($croppedImage !== false && $currentUsername) {
+        $filename = uniqid() . '.webp';
+        $filepath = '../resources/pfp/' . $filename;
 
-            $source = imagecreatefromstring($croppedImage);
+        $source = imagecreatefromstring($croppedImage);
 
-            if ($source !== false) {
-                imagewebp($source, $filepath);
-                imagedestroy($source);
+        if ($source !== false) {
+            imagewebp($source, $filepath);
+            imagedestroy($source);
 
-                $stmt = $conn->prepare("UPDATE users SET profilePicPath = ? WHERE userID = ?");
-                $stmt->bind_param("ss", $filename, $currentUsername);
-                $stmt->execute();
-                $stmt->close();
-
-                echo "Image uploaded successfully!";
-            } else {
-                echo "Error creating image resource.";
+            // Delete old profile picture if it's not the default
+            $oldProfilePicPath = '../resources/pfp/' . $currentUsername . '.webp';
+            if ($currentUsername !== 'default' && file_exists($oldProfilePicPath)) {
+                unlink($oldProfilePicPath);
             }
+
+            $stmt = $conn->prepare("UPDATE users SET profilePicPath = ? WHERE userID = ?");
+            $stmt->bind_param("ss", $filename, $currentUsername);
+            $stmt->execute();
+            $stmt->close();
+
+            echo "Image uploaded successfully!";
         } else {
-            echo "Missing data or error decoding image data.";
+            echo "Error creating image resource.";
         }
     } else {
-        echo "Invalid request method!";
+        echo "Missing data or error decoding image data.";
     }
+} else {
+    echo "Invalid request method!";
+}
 
-    $conn->close();
+$conn->close();
 ?>
